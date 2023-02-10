9f57b4dc4ae8d04d20a1dab28c2c2460cac40d21
Steven Barcus <51724784+srbarcus@users.noreply.github.com>
2023-02-09 21:08:15 -0500

    Add files via upload

--- MultiOutlet_Outlet.groovy
+++ MultiOutlet_Outlet.groovy
@@ -14,8 +14,7 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
  *  
- *  2.0.0 = Defined as a required driver
- *   
+ *  2.0.0 - Define as required driver (used as child for YoLink Devices)  
  */
 
 import groovy.json.JsonSlurper
